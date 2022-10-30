27361029a7a1b61fcd619d397379a0161409e2e0
Daniel Von Rohr <d.rudolf.von.rohr@gmail.com>
2022-10-30 21:12:21 +0100

    wip: cleanup

--- animate-block.php
+++ animate-block.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       Animate Block
+ * Plugin Name:       LivingPixel - Animate Block
  * Description:       Fade in blocks on scroll.
  * Requires at least: 6.0
  * Requires PHP:      7.0
@@ -8,7 +8,7 @@
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       animate-block
+ * Text Domain:       livingpixel-animate-block
  *
  * @package           livingpixel-blocks
  */
